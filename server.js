a55a0bdc8e69b79f81dd78eddc23001e7038e8ba
Alexandros Alex <alexntitoras@hotmail.gr>
2016-10-29 21:12:19 +0300

    UPDATE METHOD

--- server.js
+++ server.js
@@ -61,6 +61,41 @@ app.delete('/todos/:id',function (req,res) {
 
 });
 
+app.put('/todos/:id',function (req,res) {
+    var body=_.pick(req.body,'description','completed');
+    console.log(body)
+    var validAttributes={};
+    var todoId=parseInt(req.params.id,10) ;
+    var matchedTodo=_.findWhere(todos,{id:todoId});
+
+    if (!matchedTodo){
+        return res.status(404).json({"error":"no todo found with that id"});
+    }
+
+    if (body.hasOwnProperty('completed')&&_.isBoolean(body.completed)){
+        validAttributes.completed=body.completed;
+    }else if(body.hasOwnProperty('completed')){
+        return res.status(400).send();
+
+    }else{
+        //never provided attirbuted no problem here
+    }
+
+    if (body.hasOwnProperty('description')&&_.isString(body.description) && body.description.trim().length>0){
+        validAttributes.description=body.description;
+    }else if(body.hasOwnProperty('description') ){
+        return res.status(400).send();
+
+    }else{
+        //never provided attirbuted no problem here
+    }
+
+    _.extend(matchedTodo,validAttributes);
+
+    res.json(matchedTodo);
+
+});
+
 /*
 app.post('/todos/:id/delete',function (req,res) {
     var todoId=parseInt(req.params.id,10) ;
