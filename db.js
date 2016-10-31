3b2524e8bb433eda57014d2bef1d320e8166d45f
Alexandros Alex <alexntitoras@hotmail.gr>
2016-10-31 19:45:31 +0200

    Add postgres

--- db.js
+++ db.js
@@ -2,10 +2,22 @@
  * Created by alex on 31/10/2016.
  */
 var Sequelize = require('sequelize');
-var sequelize= new Sequelize(undefined,undefined,undefined,{
+var env =process.env.NODE_ENV || 'developement';
+/*var sequelize= new Sequelize(undefined,undefined,undefined,{
     'dialect':'sqlite',
     'storage':__dirname+'/data/dev-tddo-api.sqlite'
-});
+}); */
+var sequelize;
+if (env==='production'){
+    sequelize=new Sequelize(process.env.DATABASE_URL,{
+        'dialect':'postgres',
+    });
+}else {
+    new Sequelize(undefined,undefined,undefined,{
+        'dialect':'sqlite',
+        'storage':__dirname+'/data/dev-tddo-api.sqlite'
+    });
+}
 var db={};
 
 db.todo= sequelize.import(__dirname+'/models/todo.js');
