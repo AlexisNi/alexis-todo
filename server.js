473c66fb1f6adc451a7a09858d92335312d3bf62
Alexandros Alex <alexntitoras@hotmail.gr>
2016-11-01 13:40:43 +0200

    Destroy success

--- server.js
+++ server.js
@@ -72,37 +72,35 @@ app.post('/todos',function (req,res) {
 
     });
 
-
-/*    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length===0){
-        return res.status(400).send();
-    }
-
-    body.description=body.description.trim();
-    body.id=todoNextId++;
-
-    todos.push(body);
-
-    res.json(body);*/
-
 });
 
 
 app.delete('/todos/:id',function (req,res) {
     var todoId=parseInt(req.params.id,10) ;
-    var matchedTodo=_.findWhere(todos,{id:todoId});
 
-    if (!matchedTodo){
-        res.status(404).json({"error":"no todo found with that id"});
-    }else {
-        todos=_.without(todos,matchedTodo);
-        res.json(matchedTodo);
-    }
+    db.todo.destroy({
+        where:{
+            id:todoId
+              }
+    }).then(function (rowsDeleted) {
+            if(rowsDeleted===0){
+                res.status(404).json({
+                    error:'No todo with id'
+                });
+            }else {
+                res.status(204).send();
+            }
+        },function (e) {
+        res.status(500).json(e);
+    });
 
 });
 
+
+
 app.put('/todos/:id',function (req,res) {
     var body=_.pick(req.body,'description','completed');
-    console.log(body)
+    console.log(body);
     var validAttributes={};
     var todoId=parseInt(req.params.id,10) ;
     var matchedTodo=_.findWhere(todos,{id:todoId});
